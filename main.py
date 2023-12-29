37fd0aa6022e197baff602fa659cb5762648a90b
Elizaveta Melikhova <lizamelihovaa@gmail.com>
2023-12-29 13:22:09 +0200

    first commit

--- main.py
+++ main.py
@@ -1,9 +1,36 @@
 from datetime import date, datetime
+import calendar
 
 
 def get_birthdays_per_week(users):
-    # Реалізуйте тут домашнє завдання
-    return users
+    b = [day for day in calendar.day_name]
+    birthdays = {k: [] for k in b}
+    current_day = date.today()
+    filtered_birthdays = dict()
+    number_of_days = 365 if date.today().year % 4 > 0 else 366
+
+    for user in users:
+        current_year_birthday = datetime(current_day.year, user["birthday"].month, user["birthday"].day).date()
+        delta_days = int(str((current_year_birthday - current_day).days))
+        to_next_year_days = 0
+        if delta_days < 0:
+            to_next_year_days = number_of_days + delta_days
+            current_year_birthday = datetime(current_day.year+1, user["birthday"].month, user["birthday"].day).date()
+        if (delta_days >= 0) and (delta_days <= 6) or (to_next_year_days>=0) and (to_next_year_days<=6):
+            weekday_num = current_year_birthday.weekday()
+            if weekday_num < 5:
+                weekday_name = current_year_birthday.strftime('%A')
+            else:
+                weekday_name = "Monday"
+
+            name = user["name"]
+            birthdays[weekday_name].append(name)
+
+    for day, name in birthdays.items():
+        if name:
+            filtered_birthdays[day] = name
+
+    return filtered_birthdays
 
 
 if __name__ == "__main__":
